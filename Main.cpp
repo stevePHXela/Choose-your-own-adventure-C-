35956b5408dd011df9883b63c34cc38da3f73327
Steve Ela <nx4t4ty@mail.ru>
2023-02-18 20:14:04 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+#define LOG(x) cout << x << endl;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+bool quit = false;
+bool gameOver = false;
+
+
+void Draw()
+{
+	
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+
+int main()
+{
+	LOG("Test");
+}
