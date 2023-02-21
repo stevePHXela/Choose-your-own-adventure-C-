7662647348709f0444fd09474d959cc7274c85df
Steve Ela <nx4t4ty@mail.ru>
2023-02-21 21:12:06 +0300

    finished 90%

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,175 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <windows.h>
+#include <string>
+#include <array>
 
-
-#define LOG(x) cout << x << endl;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
+int level = 0;
+string message = "Enter (S) to start or enter (Q) to exit?: ";
+string situation;
+std::array<string, 2> Options;
 
-bool quit = false;
 bool gameOver = false;
 
+struct Keys
+{
+	static const char QUIT = 'q';
+	static const char CHOICE_A = 'a';
+	static const char CHOICE_B = 'b';
+};
 
 void Draw()
 {
+	system("cls");
+
+	cout << "###########################################" << endl;
+	cout << "Welcome to, choose your own adventure game!" << endl;
+	cout << "###########################################" << endl;
+
+	if (level > 0)
+	{
+		cout << endl;
+		cout << situation << endl;
+		cout << Options[0] << endl;
+		cout << Options[1] << endl;
+		cout << endl;
+	}
 	
+	cout << message;
 }
 
-void Input()
+char getUserInput()
 {
+	char userInput;
+	cin >> userInput;
 
+	if (userInput == Keys::QUIT)
+	{
+		exit(EXIT_SUCCESS);
+	}
+
+	return userInput;
 }
 
 void Logic()
 {
+	char userChoice = std::tolower(getUserInput());
 
+	if (level == 0)
+	{
+		message = "Enter a choices or enter (Q) to exit?: ";
+		situation = "They are here. their vision is based on movement, they give chase.";
+		Options[0] = "A. Give in!";
+		Options[1] = "B. Run!";
+		level++;
+	}
+
+	if (level == 1 && userChoice != 's')
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			message = "They show you no mercy. The pain is indescribable.";
+			gameOver = true;
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{
+			level++;
+			situation = "They are fast, too fast. They are catching up.";
+			Options[0] = "A. Turn and fight!";
+			Options[1] = "B. Hide!";
+		}
+	}
+	else if (level == 2)
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			message = "You fool! They quickly overpower you. Your end is neither swift nor dignified. Your mangled corpse serves as a warning to others.";
+			gameOver = true;
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{
+			level++;
+			situation = "There is a pile of rubble you can squeeze under. A metal object glints faintly.";
+			Options[0] = "A. Examine!";
+			Options[1] = "B. Stay hidden!";
+		}
+	}
+	else if (level == 3)
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			message = " All that glisters is not gold. The detonation rips you in half. They feast on your remains.";
+			gameOver = true;
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{
+			level++;
+			situation = "Your heart beats loudly, you are sure they will hear. An hour passes.";
+			Options[0] = "A. Wait!";
+			Options[1] = "B. Look around!";
+		}
+	}
+	else if (level == 4)
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			level++;
+			situation = "Suddenly you jerk awake! How much time has passed? It is night.";
+			Options[0] = "A. Leave!";
+			Options[1] = "B. Stay hidden!";
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{			
+			message = "You stepped into a trap, They cut you in a parts, The pain is indescribable.";
+			gameOver = true;
+		}
+	}
+	else if (level == 5)
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			level++;
+			situation = "They have gone. Slowly, you creep out. Blood is smeared on the streets. You are tired.";
+			Options[0] = "A. Scream for help!";
+			Options[1] = "B. Keep moving!";
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{			
+			message = "The night is bitterly cold. In the morning, they find your frozen body. They snap your fingers off for fun.";
+			gameOver = true;
+		}
+	}
+	else if (level == 6)
+	{
+		if (userChoice == Keys::CHOICE_A)
+		{
+			message = "They heard you. Its foul breath fills you with corruption. You deserve this. You truly do for your wicked life.";
+			gameOver = true;
+		}
+		else if (userChoice == Keys::CHOICE_B)
+		{			
+			message = "Congratulations!!! You found a way out of the island!";
+			gameOver = true;
+		}
+	}
 }
 
+void Run()
+{
+	while (!gameOver)
+	{
+		Draw();
+		Logic();
+	}
+}
 
 int main()
 {
-	LOG("Test");
+	Run();
 }
